2890425230223ec12a4ef3455ca2a7f2e4790bdc
Mauri <hello@mauriciabad.com>
2018-10-20 16:25:09 +0200

    ok

---
+++ script.js
@@ -0,0 +1,5 @@
+function startSurvey() {
+  document.getElementsByClassName("home-section")[0].style.opacity = 0;
+  document.getElementsByTagName("header")[0].style.opacity = 0;
+  location.assign("https://picsoung.typeform.com/to/DBVdEq");
+}
