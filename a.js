41e5e43aa763f48fde554361f2218f125494655f
Mauri <hello@mauriciabad.com>
2018-10-20 22:58:06 +0200

    v1

---
+++ a.js
@@ -0,0 +1,34 @@
+var recipts = [
+  {
+    "name": "Recepta 1",
+    "ingredients": ["chicken", "green_pepper","milk"],
+    "amounghts": [1,0,0],
+    "grams": [0,200,0],
+    "mililitters": [0,0,500],
+    }
+  }
+];
+
+var ingredients = {
+  "Ing1": {
+    "healthy": 85
+  },
+  "Ing2": {
+    "healthy": 50
+  },
+  "Ing3": {
+    "healthy": 15
+  }
+};
+
+var users = {
+  "grandma": {
+    "is":["Vegan","Vegetarian"],
+    "dislikes":["Pasta","Pizza"]
+  }
+};
+
+var categories = {
+  "Vegan": ["Meat","Eggs"],
+  "Vegetarian": ["Meat"]
+};
--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 function startSurvey() {
-  document.getElementsByClassName("home-section")[0].style.opacity = 0;
+  document.getElementById("section-profile").style.opacity = 0;
   document.getElementsByTagName("header")[0].style.opacity = 0;
+  document.getElementsByTagName("footer")[0].style.opacity = 0;
   location.assign("https://picsoung.typeform.com/to/DBVdEq");
+}
+
+function login(name) {
+  showSection('diet');
+  getMenuFromUser(name);
+}
+
+function showSection(name) {
+  for (let section of document.getElementsByClassName('section-active')) {    
+    section.classList.remove('section-active');
+    section.classList.add('section-hidden');  
+  }
+  document.getElementById("section-"+name).classList.remove('section-hidden');  
+  document.getElementById("section-"+name).classList.add('section-active');  
+
+  for (let footer of document.getElementsByClassName('footer-active')) {    
+    footer.classList.remove('footer-active');
+  }
+  document.getElementById("footer-"+name).classList.add('footer-active');  
+}
+function getMenuFromUser(name) {
+  //TODO ajax
 }
